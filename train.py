9c734f2ec886382a0886deefa4c20a989e9b456c
ALGUBELLI MANIDEEP REDDY <164165949+Manideep-422105@users.noreply.github.com>
2025-10-12 10:24:20 +0530

    commit2

--- train.py
+++ train.py
@@ -154,7 +154,7 @@ def train(model_list, optimizer_list, train_loader, scheduler_list):
         print("model:{} best acc:{:.2f}".format(args.model_names[k], best_acc[k]))
 
 
-if _name_ == '_main_':
+if __name__ == '_main_':
     train_loader, val_loader = get_dataloader(args)
     model_list = []
     optimizer_list = []
